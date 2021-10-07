88865ada5b1a30f2de2b7cd429943a9438825781
Charlie Ford <charles.ford1978@gmail.com>
2021-10-07 16:08:59 -0400

    Added OrdersController class and added code to Main program to test it.

--- EntityFramework-codefirst/Program.cs
+++ EntityFramework-codefirst/Program.cs
@@ -8,6 +8,10 @@ namespace EntityFramework_codefirst
     {
         async static Task Main(string[] args)
         {
+            var ordCtrl = new OrdersController();
+            var orders = await ordCtrl.GetAll();
+            orders.ForEach(o => Console.WriteLine($"{o.Description} | {o.Customer.Name}"));
+
             var custCtrl = new CustomersController();
             var customers = await custCtrl.GetAll();
             customers.ForEach(c => Console.WriteLine(c.Name));
---
+++ SalesDbLib/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesDbLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDbLib.Controllers
+{
+    public class OrdersController
+    {
+        private readonly SalesDbContext _context;
+
+        public OrdersController()
+        {
+            _context = new SalesDbContext();
+        }
+        
+        public async Task<List<Order>> GetAll()
+        {
+            return await _context.Orders
+                                       .Include(x => x.Customer) 
+                                       .ToListAsync();
+        }
+    }
+}
