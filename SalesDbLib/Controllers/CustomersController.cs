a3ceddd03c1b4ece58bc4a7c0a7992fb498c7846
Charlie Ford <charles.ford1978@gmail.com>
2021-10-07 14:49:40 -0400

    Add project files.

---
+++ SalesDbLib/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesDbLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDbLib.Controllers
+{
+    public class CustomersController
+    {
+        private readonly SalesDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new SalesDbContext();
+        }
+        public async Task<List<Customer>> GetAll()
+        {
+            return await _context.Customers
+                                    .OrderBy(o => o.Name)
+                                    .ToListAsync();
+        }
+
+    }
+}
---
+++ SalesDbLib/Models/SalesDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDbLib.Models
+{
+    public class SalesDbContext : DbContext
+    {
+        // DbSets go here
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Customer>(e =>
+            {
+                e.HasIndex(p => p.Code).IsUnique(true);//passing in 'true' is not
+                                                       //necessary. it will default
+                                                       //to true.  Just showing for
+                                                       //illustration purposes.
+                e.Property(p => p.Name).HasMaxLength(30).
+                IsRequired(true);//this does the same thing we did with attributes in
+                                 //Customer class.  This fluent api will override
+                                 //the attributes though.
+            });
+        }
+        
+        public SalesDbContext() {}
+
+        public SalesDbContext(DbContextOptions<SalesDbContext> options)
+            : base(options)
+        {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                var connStr = "server=localhost\\sqlexpress;database=EfSalesDb;" +
+                    " trusted_connection=true;";
+                builder.UseSqlServer(connStr);
+            }
+        }
+    }
+}
