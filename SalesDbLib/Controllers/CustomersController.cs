dabf8641943da8ef6efec78475789520cd0bd82b
Charlie Ford <charles.ford1978@gmail.com>
2021-10-07 23:44:14 -0400

    Added GetbyPk, Create, Change and Remove methods in both Controllers.  Made the methods in CustomersController asyncronous.

--- SalesDbLib/Controllers/CustomersController.cs
+++ SalesDbLib/Controllers/CustomersController.cs
@@ -22,6 +22,48 @@ namespace SalesDbLib.Controllers
                                     .OrderBy(o => o.Name)
                                     .ToListAsync();
         }
-
+        public async Task<Customer> GetbyPk(int Id)
+        {
+            return await _context.Customers.FindAsync(Id);
+        }
+        public async Task<bool> Create(Customer customer)
+        {
+            if(customer.Id != 0)
+            {
+                throw new Exception("You must enter 0 for Id!");
+            }
+            await _context.Customers.AddAsync(customer);
+            var rowsAffected = await _context.SaveChangesAsync();
+            if(rowsAffected != 1)
+            {
+                throw new Exception("Create failed!");
+            }
+            return true;
+        }
+        public async Task<bool> Change(int Id, Customer customer)
+        {
+            if(Id != customer.Id)
+            {
+                throw new Exception("Id's do not match!");
+            }
+            _context.Entry(customer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            var rowsAffected = await _context.SaveChangesAsync();
+            if(rowsAffected != 1)
+            {
+                throw new Exception("Update failed!");
+            }
+            return true;
+        }
+        public async Task<bool> Remove(int Id)
+        {
+            var customer = await _context.Customers.FindAsync(Id);
+            if(customer == null)
+            {
+                return false;
+            }
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
--- SalesDbLib/Controllers/OrdersController.cs
+++ SalesDbLib/Controllers/OrdersController.cs
@@ -23,5 +23,48 @@ namespace SalesDbLib.Controllers
                                        .Include(x => x.Customer) 
                                        .ToListAsync();
         }
+        public Order GetbyPk(int Id)
+        {
+            return _context.Orders.Find(Id);
+        }
+        public bool Create(Order order)
+        {
+            if(order.Id != 0)
+            {
+                throw new Exception("You must enter 0 for Id!");
+            }
+            _context.Orders.Add(order);
+            var rowsAffected = _context.SaveChanges();
+            if(rowsAffected != 1)
+            {
+                throw new Exception("Create failed!");
+            }
+            return true;
+        }
+        public bool Change(int Id, Order order)
+        {
+            if(Id != order.Id)
+            {
+                throw new Exception("Id's do not match!");
+            }
+            _context.Entry(order).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            var rowsAffected = _context.SaveChanges();
+            if(rowsAffected != 1)
+            {
+                throw new Exception("Update failed!");
+            }
+            return true;
+        }
+        public bool Remove(int Id)
+        {
+            var order = _context.Orders.Find(Id);
+            if(order == null)
+            {
+                return false;
+            }
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+            return true
+        }
     }
 }
